994eeb7aca0e110b7a114e41034c7b770fab2309
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-01-31 17:18:46 +0100

    creati livelli di difficoltà

--- main.js
+++ main.js
@@ -1,23 +1,49 @@
-const gridElement = document.getElementById('grid');
-
-const createGridElement = () => {
+/*Funzioni*/
+const createGridElement = (classe) => {
     const node = document.createElement('div');
     node.classList.add('square');
+    node.classList.add(classe);
     return node;
 }
+/*variabili*/
+const gridElement = document.getElementById('grid');
+
+let buttonPlay = document.getElementById("play");
 
 
-for (let i = 1; i<=100; i++) {
 
-    const node = createGridElement();
+buttonPlay.addEventListener('click',
+    function(){
+        let level = document.getElementById("level").value;
+        let numBox;
+        let size ;
 
-    node.innerHTML += i;
-    
-    node.addEventListener('click', function() {
-        console.log(this);
-        this.classList.add('clicked');
-    });
+        if(level == "Easy"){
+            numBox = 100;
+            size = "easy";
+        }else if (level == "Hard"){
+            numBox = 81;
+            size = "hard";
+        }else if (level == "Crazy"){
+            numBox = 49;
+            size = "crazy";
+        }
 
-    gridElement.appendChild(node);
+        //creazione dei quadrati nell'HTML
+        for (let i = 1; i<=numBox; i++) {
 
-}
+        const node = createGridElement(size);
+
+        node.innerHTML += i;
+
+        node.addEventListener('click', function() {
+            console.log(this);
+            this.classList.add('clicked');
+        });
+
+        gridElement.appendChild(node);
+
+        }
+    }
+);
+
